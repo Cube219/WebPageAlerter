5f4013862eda352d1e38b7442bd9d01c8f8fb327
Cube219 <max804800@gmail.com>
2019-06-16 20:25:55 +0900

    Migrate from express to koa

--- src/APIServer.ts
+++ src/APIServer.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import helmet from "helmet";
-import bodyParser from "body-parser";
+import koa from "koa";
+import koaRouter from "koa-router";
+import koaHelmet from "koa-helmet";
+import koaBodyParser from "koa-bodyparser";
 
 import http from "http";
 import http2 from "http2";
-import spdy from "spdy";
 
 import fs from "fs";
 import { Log } from "./Log";
@@ -20,25 +20,26 @@ export interface APIServerInitializer
 
 export class APIServer
 {
-    private expressApp: express.Express;
+    private koaApp: koa;
     private port: number;
 
-    private spdyServer: spdy.Server;
     private httpServer: http.Server;
+    private server: http2.Http2SecureServer;
 
     constructor(init: APIServerInitializer)
     {
         this.port = init.port;
 
-        this.expressApp = express();
-        this.initExpress();
+        this.koaApp = new koa();
+        this.initKoa();
 
         const options = {
             key: fs.readFileSync(init.keyPath),
-            cert: fs.readFileSync(init.certPath)
+            cert: fs.readFileSync(init.certPath),
+            allowHTTP1: true
         }
-        // http2.createSecureServer(options, this.expressApp).listen(this.port);
-        this.spdyServer = spdy.createServer(options, this.expressApp);
+
+        this.server = http2.createSecureServer(options, this.koaApp.callback());
 
         // Redirect from http to https
         this.httpServer = http.createServer(function(req, res) {
@@ -52,187 +53,188 @@ export class APIServer
         this.httpServer.listen(80, ()=> {
             Log.info("Started http server for redirecting to https.")
         });
-        this.spdyServer.listen(443, ()=> {
+        this.server.listen(443, () => {
             Log.info("Started APIServer.");
         });
     }
 
     public stop()
     {
-        this.spdyServer.close();
+        this.server.close();
         this.httpServer.close();
     }
 
-    private initExpress()
+    private initKoa()
     {
-        this.expressApp.use(helmet());
-        this.expressApp.use(helmet.contentSecurityPolicy({
+        this.koaApp.use(async (ctx, next) => {
+            try {
+                await next();
+            } catch (err) {
+                ctx.status = 500;
+                ctx.app.emit("error", err, ctx);
+            }
+        });
+        this.koaApp.on("error", (err, ctx) => {
+            Log.error(`Error in ${ctx.request.method}:${ctx.request.url}\n        ${err.stack}`);
+        });
+
+        this.koaApp.use(koaHelmet());
+        this.koaApp.use(koaHelmet.contentSecurityPolicy({
             directives: {
                 defaultSrc: ["'self'"]
             }
-        }))
+        }));
 
-        this.expressApp.use(bodyParser.urlencoded({ extended: false}));
-        this.expressApp.use(bodyParser.json());
+        this.koaApp.use(koaBodyParser());
 
-        const router = express.Router();
+        const router = new koaRouter();
+        
+        router.get("/pages", this.getPages);
+        router.delete("/page/:id", this.deletePage);
+        router.put("/page/read/:id", this.markPageAsRead);
 
-        router.route("/pages").get(this.getPages);
-        router.route("/page/:id").delete(this.deletePage);
-        router.route("/page/read/:id").put(this.markPageAsRead);
+        router.get("/sites", this.getSites);
+        router.post("/site", this.addSite);
+        router.put("/site/:id", this.updateSite);
+        router.delete("/site/:id", this.deleteSite);
 
-        router.route("/sites").get(this.getSites);
-        router.route("/site").post(this.addSite);
-        router.route("/site/:id").put(this.updateSite);
-        router.route("/site/:id").delete(this.deleteSite);
-
-        this.expressApp.use("/", router);
+        this.koaApp.use(router.routes());
+        this.koaApp.use(router.allowedMethods());
     }
 
     // Routing functions
-    private getPages(req: express.Request, res: express.Response)
+    private async getPages(ctx: koa.ParameterizedContext, next: () => Promise<any>)
     {
-        const params = req.query;
+        const params = ctx.query;
 
         const startIndex = parseInt(params.startIndex);
         if(startIndex < 0) {
-            res.sendStatus(400);
+            ctx.response.status = 400;
             return;
         }
+
+        try {
+            const r = await DB.getPages({
+                onlyUnread: (params.onlyUnread == "true"),
+                category: params.category,
+                startIndex: startIndex,
+                count: parseInt(params.count)
+            });
+
+            ctx.response.status = 200;
+            ctx.body = r;
+        } catch(e) {
+            e.message += `\n        Request parameters: ${JSON.stringify(params)}`;
+            throw e;
+        }
+    }
+
+    private async deletePage(ctx: koa.ParameterizedContext, next: () => Promise<any>)
+    {
+        try {
+            const res = await DB.deletePage(ctx.params.id);
+            
+            if(res == 0) {
+                Log.warn(`Tried to delete the page '${ctx.params.id}', but could not find.`);
+            }
+
+            ctx.status = 204;
+        } catch(e) {
+            e.message += `${e}\n        Page id: ${ctx.params.id}`;
+            throw e;
+        }
+    }
+
+    private async markPageAsRead(ctx: koa.ParameterizedContext, next: () => Promise<any>)
+    {
+        try {
+            const res = await DB.readPage(ctx.params.id);
+
+            if(res == 0) {
+                Log.warn(`Tried to read the page '${ctx.params.id}', but could not find.`);
+            }
+
+            ctx.status = 204;
+        } catch(e) {
+            e.message += `${e}\n        Page id: ${ctx.parmas.id}`;
+            throw e;
+        }
+    }
+
+    private async getSites(ctx: koa.ParameterizedContext, next: () => Promise<any>)
+    {
+        const res = await DB.getWebSites();
         
-        DB.getPages({
-            onlyUnread: (params.onlyUnread == "true"),
-            category: params.category,
-            startIndex: startIndex,
-            count: parseInt(params.count)
-        }).then((r) => {
-            res.status(200).json(r);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to get the pages. (${e})`);
-            Log.error(`    Request parameters: ${JSON.stringify(params)}`);
-        });
+        ctx.status = 200;
+        ctx.body = res;
     }
 
-    private deletePage(req: express.Request, res: express.Response)
+    private async addSite(ctx: koa.ParameterizedContext, next: () => Promise<any>)
     {
-        DB.deletePage(req.params.id).then((r) => {
-            if(r == 0){
-                Log.warn(`Tried to delete the page '${req.params.id}', but could not find.`);
-            }
-
-            res.sendStatus(204);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to delete the page. (${e})`);
-            Log.error(`    Page id: ${req.params.id}`);
-        });
-    }
-
-    private markPageAsRead(req: express.Request, res: express.Response)
-    {
-        DB.readPage(req.params.id).then((r) => {
-            if(r == 0){
-                Log.warn(`Tried to read the page '${req.params.id}', but could not find.`);
-            }
-
-            res.sendStatus(204);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to read the page. (${e})`);
-            Log.error(`    Page id: ${req.params.id}`);
-        });
-    }
-
-    private getSites(req: express.Request, res: express.Response)
-    {
-        DB.getWebSites().then((r) => {
-            res.status(200).json(r);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to get the web sites. (${e})`);
-        });
-    }
-
-    private addSite(req: express.Request, res: express.Response)
-    {
-        const params = req.body;
+        const params = ctx.request.body;
 
         if(!params.title || !params.url || !params.crawlUrl || !params.cssSelector) {
-            res.sendStatus(400);
+            ctx.status = 400;
             return;
         }
         if(!params.category) {
             params.category = "general";
         }
 
-        DB.insertWebSite({
-            title: params.title,
-            url: params.url,
-            crawlUrl: params.crawlUrl,
-            cssSelector: params.cssSelector,
-            category: params.category,
-            lastTitle: ""
-        }).then((r) => {
-            res.sendStatus(204);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
+        try {
+            await DB.insertWebSite({
+                title: params.title,
+                url: params.url,
+                crawlUrl: params.crawlUrl,
+                cssSelector: params.cssSelector,
+                category: params.category,
+                lastTitle: ""
+            });
 
-            Log.error(`Failed to add the web site. (${e})`);
-            Log.error(`    Request parameters: ${JSON.stringify(params)}`);
-        });
+            ctx.status = 204;
+        } catch(e) {
+            e.message += `${e}\n        Request parameters: ${JSON.stringify(params)}`;
+            throw e;
+        }
     }
 
-    private updateSite(req: express.Request, res: express.Response)
+    private async updateSite(ctx: koa.ParameterizedContext, next: () => Promise<any>)
     {
-        const params = req.body;
+        const params = ctx.request.body;
         
-        DB.updateWebSite(req.params.id, {
-            crawlUrl: params.crawlUrl,
-            cssSelector: params.cssSelector,
-            category: params.category
-        }).then((r) => {
-            if(r == 0){
-                Log.warn(`Tried to update the page '${req.params.id}', but could not find.`);
+        try {
+            const res = await DB.updateWebSite(ctx.params.id, {
+                crawlUrl: params.crawlUrl,
+                cssSelector: params.cssSelector,
+                category: params.category
+            });
+
+            if(res == 0) {
+                Log.warn(`Tried to update the page '${ctx.params.id}', but could not find.`);
             }
 
-            res.sendStatus(204);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to update the web site. (${e})`);
-            Log.error(`    Web site id: ${req.params.id}`);
-            Log.error(`    Request parameters: ${JSON.stringify(params)}`);
-        });
+            ctx.status = 204;
+        } catch(e) {
+            e.message += `${e}\n        Web site id: ${ctx.params.id}\n    Request parameters: ${JSON.stringify(params)}`
+            throw e;
+        }
     }
 
-    private deleteSite(req: express.Request, res: express.Response)
+    private async deleteSite(ctx: koa.ParameterizedContext, next: () => Promise<any>)
     {
-        const params = req.body;
+        const params = ctx.request.body;
         
-        DB.deleteWebSite(req.params.id, (params.deleteAllPages == "true")).then((r) => {
-            if(r == 0) {
-                Log.warn(`Tried to delete the page '${req.params.id}', but could not find.`);
+        try {
+            const res = await DB.deleteWebSite(ctx.params.id, (params.deleteAllPages == "true"));
+
+            if(res == 0) {
+                Log.warn(`Tried to delete the page '${ctx.params.id}', but could not find.`);
             }
 
-            res.sendStatus(204);
-        })
-        .catch((e) => {
-            res.sendStatus(500);
-
-            Log.error(`Failed to delete the web site. (${e})`);
-            Log.error(`    Web site id: ${req.params.id}`);
-            Log.error(`    Request parameters: ${JSON.stringify(params)}`);
-        })
+            ctx.state = 204;
+        } catch(e) {
+            e.message += `${e}\n        Web site id: ${ctx.params.id}\n    Request parameters: ${JSON.stringify(params)}`;
+            throw e;
+        }
     }
 }
