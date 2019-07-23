6bc3d52fcb14b1e595ff4e46586d8297ec2b6bf6
Cube219 <max804800@gmail.com>
2019-07-23 20:41:59 +0900

    Fixed loading incorrect archieved pages using afterId

--- src/APIServer.ts
+++ src/APIServer.ts
@@ -259,6 +259,7 @@ export class APIServer
                 onlyUnread: (params.onlyUnread == "true"),
                 category: params.category,
                 startIndex: startIndex,
+                afterId: params.afterId,
                 count: parseInt(params.count)
             }, true);
 
